6cc97c6327bc3e98ebeca0d3293012a21470fd44
Yodaaa <129697705+Mamayoda@users.noreply.github.com>
2024-05-07 23:28:29 -0400

    se agrego factory
    para crear nuevos luchadores

---
+++ FabricaDeLuchadores.java
@@ -0,0 +1,17 @@
+public class FabricaDeLuchadores {
+    // Método estático para crear instancias de luchadores según el tipo
+    public static Object createFighter(String fighterType) {
+        if (fighterType.equals("1")) {
+            // Crea una instancia de ChoroPortenho
+            return new ChoroPortenho();
+        } else if (fighterType.equals("2")) {
+            // Crea una instancia de MineroWarrior
+            return new MineroWarrior();
+        } else {
+            // Manejar tipos no válidos, puede lanzar una excepción o retornar null
+            return null;
+        }
+    }    
+    
+}
+
--- FightEngine.java
+++ FightEngine.java
@@ -1,71 +1,81 @@
 import java.util.Scanner;
 
 public class FightEngine {
-	public FightEngine(){
-		String luchadorPlayer1;
-		String accionLuchadorIn;
-		int accionLuchador;
-		String opcionChoroPortenho = new String("1");
-		String opcionMineroWarrior = new String("2");
-		Dificultad dificultad = new Dificultad(100);
-			
-	    System.out.println("Empieza el juego!!!");
-		System.out.println("Selecciona tu luchador (1 o 2): 1-Choro Portenho | 2-Minero Warrior ");
-		
-		Scanner seleccion = new Scanner(System.in);
-		luchadorPlayer1 = seleccion.nextLine();
-	
-		System.out.println(luchadorPlayer1);
-		
-		if (luchadorPlayer1.equals(opcionChoroPortenho)){
-				System.out.println("Choro Portenho inicia a batalla!");
-				ChoroPortenho player1CP = new ChoroPortenho();
-				dificultad.imprimirVidaEnemigo();
-				while (dificultad.obtenerVida()>0){
-					System.out.println("Ingresa tu accion! :1-Golpe | 2-Patada | 3-Salto | 4-LANZAR PODER");
-					accionLuchadorIn = seleccion.nextLine();
-					accionLuchador = Integer.parseInt(accionLuchadorIn);
-					switch(accionLuchador) {
-						case 1: System.out.println(player1CP.golpear());
-								dificultad.reducirVida(10);
-								break;
-						case 2: System.out.println(player1CP.patear());
-								dificultad.reducirVida(20);
-								break;
-						case 3: System.out.println(player1CP.saltar());
-								dificultad.reducirVida(0);
-								break;
-						case 4: System.out.println(player1CP.LanzarPoder());
-								dificultad.reducirVida(50);
-								break;
-					}
-				}
-		}
-		
-		if (luchadorPlayer1.equals(opcionMineroWarrior)){
-			System.out.println("Minero Warrior inicia a batalla!");
-			
-			MineroWarrior player1MW = new MineroWarrior();
-			
-			System.out.println("Ingresa tu accion! :1-Golpe 2-Patada 3-Salto 4-LANZAR PODER");
-			accionLuchadorIn =seleccion.nextLine();
-			accionLuchador = Integer.parseInt(accionLuchadorIn);
-			
-			switch(accionLuchador) {
-				case 1: System.out.println(player1MW.golpear());
-						break;
-				case 2: System.out.println(player1MW.patear());
-						break;
-				case 3: System.out.println(player1MW.saltar());
-						break;
-				case 4: System.out.println(player1MW.LanzarPoder());
-						break;
-			}		
-		
-		}
-		
-		System.out.println("FIN DEL JUEGO. 2...");
-
-	}
-
+    public FightEngine() {
+        String luchadorPlayer1;
+        String accionLuchadorIn;
+        int accionLuchador;
+        Dificultad dificultad = new Dificultad(100);
+            
+        System.out.println("Empieza el juego!!!");
+        System.out.println("Selecciona tu luchador (1 o 2): 1-Choro Portenho | 2-Minero Warrior ");
+        
+        Scanner seleccion = new Scanner(System.in);
+        luchadorPlayer1 = seleccion.nextLine();
+    
+        System.out.println(luchadorPlayer1);
+        
+        // Usa FighterFactory para crear el luchador seleccionado
+        Object player1 = FabricaDeLuchadores.createFighter(luchadorPlayer1);
+        
+        if (player1 != null) {
+            System.out.println(player1 instanceof ChoroPortenho ? "Choro Portenho inicia la batalla!" : "Minero Warrior inicia la batalla!");
+            dificultad.imprimirVidaEnemigo();
+            
+            while (dificultad.obtenerVida() > 0) {
+                System.out.println("Ingresa tu acción! :1-Golpe | 2-Patada | 3-Salto | 4-LANZAR PODER");
+                accionLuchadorIn = seleccion.nextLine();
+                accionLuchador = Integer.parseInt(accionLuchadorIn);
+                
+                if (player1 instanceof ChoroPortenho) {
+                    ChoroPortenho player1CP = (ChoroPortenho) player1;
+                    switch (accionLuchador) {
+                        case 1:
+                            System.out.println(player1CP.golpear());
+                            dificultad.reducirVida(10);
+                            break;
+                        case 2:
+                            System.out.println(player1CP.patear());
+                            dificultad.reducirVida(20);
+                            break;
+                        case 3:
+                            System.out.println(player1CP.saltar());
+                            dificultad.reducirVida(0);
+                            break;
+                        case 4:
+                            System.out.println(player1CP.LanzarPoder());
+                            dificultad.reducirVida(50);
+                            break;
+                    }
+                } else if (player1 instanceof MineroWarrior) {
+                    MineroWarrior player1MW = (MineroWarrior) player1;
+                    switch (accionLuchador) {
+                        case 1:
+                            System.out.println(player1MW.golpear());
+                            dificultad.reducirVida(10);
+                            break;
+                        case 2:
+                            System.out.println(player1MW.patear());
+                            dificultad.reducirVida(20);
+                            break;
+                        case 3:
+                            System.out.println(player1MW.saltar());
+                            dificultad.reducirVida(0);
+                            break;
+                        case 4:
+                            System.out.println(player1MW.LanzarPoder());
+                            dificultad.reducirVida(50);
+                            break;
+                    }
+                }
+                
+                dificultad.imprimirVidaEnemigo();
+            }
+        } else {
+            System.out.println("Opción de luchador inválida. Fin del juego.");
+        }
+        
+        seleccion.close();
+        System.out.println("FIN DEL JUEGO.");
+    }
 }
